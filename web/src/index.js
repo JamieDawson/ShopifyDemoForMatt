914b3962795199499ddf548f3a8664e697c5a8a6
JamieDawson <jamierobertdawson@gmail.com>
2020-10-26 18:12:14 -0700

    all

---
+++ web/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Client from 'shopify-buy';
+//import '../../shared/app.css';
+
+const client = Client.buildClient({
+	storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
+	domain: 'graphql.myshopify.com',
+});
+
+ReactDOM.render(<App client={client} />, document.getElementById('root'));
